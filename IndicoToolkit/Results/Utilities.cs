738f10894b5cd50fca63ec778437b38d78c02613
Michael Welborn <michael.welborn@indicodata.ai>
2025-03-11 15:49:11 -0500

    Add support for optional types in `Utils.Get` and `Utils.Has`

--- IndicoToolkit/Results/Utilities.cs
+++ IndicoToolkit/Results/Utilities.cs
@@ -1,5 +1,4 @@
 using Newtonsoft.Json.Linq;
-using System.Linq;
 
 namespace IndicoToolkit.Results;
 
@@ -8,8 +7,23 @@ public static class Utils
 {
     // Get the value of `key` from `json` if it exists and is of type `ValueType`.
     // Raise an exception otherwise.
-    public static ValueType Get<ValueType>(JToken json, string key)
+    public static ValueType Get<ValueType>(JToken? json, string key)
     {
+        if (json == null || json[key] == null || json[key].Type == JTokenType.Null)
+        {
+            var type = typeof(ValueType);
+            var isNullableValueType = Nullable.GetUnderlyingType(type) != null;
+            var isReferenceType = !type.IsValueType;
+
+            if (isNullableValueType || isReferenceType)
+                return default;
+            else
+                throw new ResultException(
+                    $"JSON object `{json}` does not have a value for "
+                    + $"key `{key}` with type `{typeof(ValueType)}`"
+                );
+        }
+
         try
         {
             return json[key].ToObject<ValueType>();
@@ -24,11 +38,12 @@ public static class Utils
     }
 
     // Determine whether `json` has `key` with a value of type `ValueType`.
-    public static bool Has<ValueType>(JToken json, string key)
+    public static bool Has<ValueType>(JToken? json, string key)
     {
         try
         {
-            return json[key].ToObject<ValueType>() != null;
+            Get<ValueType>(json, key);
+            return true;
         }
         catch (System.Exception)
         {
