1eb36cf6735ef95ae8db086fbf6ac78287d385fa
Michael Welborn <michael.welborn@indicodata.ai>
2025-05-07 17:47:40 -0500

    Remove unused functions

--- IndicoToolkit/Results/Predictions/Box.cs
+++ IndicoToolkit/Results/Predictions/Box.cs
@@ -52,18 +52,6 @@ public record Box
         );
     }
 
-    public JObject ToJson()
-    {
-        return new()
-        {
-            ["page_num"] = Page,
-            ["top"] = Top,
-            ["left"] = Left,
-            ["right"] = Right,
-            ["bottom"] = Bottom,
-        };
-    }
-
     // It's more ergonomic to represent the lack of a bounding box with a special null
     // box object rather than using `null` or raising an error. This lets you e.g. sort
     // by the `box` attribute without having to constantly check for `null`, while
