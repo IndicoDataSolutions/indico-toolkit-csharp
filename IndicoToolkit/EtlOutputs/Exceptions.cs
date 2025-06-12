bfd7483b4d172b21b230d07286e1a5bafed6b8e4
Michael Welborn <michael.welborn@indicodata.ai>
2025-06-12 11:36:50 -0500

    Comment Exceptions

--- IndicoToolkit/EtlOutputs/Exceptions.cs
+++ IndicoToolkit/EtlOutputs/Exceptions.cs
@@ -1,18 +1,27 @@
 namespace IndicoToolkit.EtlOutputs;
 
 
+/*
+Thrown when an error occurs accessing `EtlOutput` values.
+*/
 public class EtlOutputException : System.Exception
 {
     public EtlOutputException(string message) : base(message) { }
 }
 
 
+/*
+Thrown when a `Token` can't be found for a `Span`.
+*/
 public class TokenNotFoundException : EtlOutputException
 {
     public TokenNotFoundException(string message) : base(message) { }
 }
 
 
+/*
+Thrown when a `Table` and `Cell` can't be found for a `Token`.
+*/
 public class TableCellNotFoundException : EtlOutputException
 {
     public TableCellNotFoundException(string message) : base(message) { }
